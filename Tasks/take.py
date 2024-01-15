cc0779214ecf2cf444730237c9bb58b3b1998f10
Ivan <yvan.avr1702@gmail.com>
2024-01-15 12:55:16 +0200

    Don't mutate incoming parameters

--- Tasks/take.py
+++ Tasks/take.py
@@ -1,7 +1,8 @@
 # Copy all listed keys from dictionary
 
 def take(input_dict, *target_keys):
+    result = dict(input_dict)
     for key in input_dict:
         if key not in target_keys:
-            del input_dict[key]
-    return input_dict
+            del result[key]
+    return result
