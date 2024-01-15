2963dc109cc2c782061a3e8f163cd182956ac24b
Ivan <yvan.avr1702@gmail.com>
2024-01-15 11:59:04 +0200

    Remove senseless code

--- Tasks/filter.py
+++ Tasks/filter.py
@@ -1,15 +1,11 @@
 # Filter array by type name
 
 def filter(input_arr, type_string):
-    remove = []
     result = input_arr[:]
 
-    for element in result:
-        el_index = result.index(element)
-        if not isinstance(result[el_index], eval(type_string)):
-            remove.insert(0, el_index)
+    for element in reversed(result):
+        if not isinstance(element, eval(type_string)):
+            result.remove(element)
 
-    for element in remove:
-        result.pop(element)
 
     return result
