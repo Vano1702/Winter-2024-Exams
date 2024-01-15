835c5ee0ce18c5994eff5508bd6562d87e5262a7
Ivan <yvan.avr1702@gmail.com>
2024-01-15 09:35:12 +0200

    Changed constants to simplify code

--- Tasks/day.py
+++ Tasks/day.py
@@ -1,9 +1,9 @@
 # Get day number
 
-DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
 
 def parse_day(day_string):
     for i in range(len(DAYS)):
-        if day_string.startswith(DAYS[i].lower()):
+        if day_string.startswith(DAYS[i]):
             return i + 1
     return -1
