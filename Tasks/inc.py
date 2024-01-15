d7575adf7a1b3c07131bf736d840555abe38394c
Ivan <yvan.avr1702@gmail.com>
2024-01-15 12:11:22 +0200

    Remove unused and senseless code

--- Tasks/inc.py
+++ Tasks/inc.py
@@ -1,8 +1,8 @@
 # Increment all numbers in dictionary
 
-def inc_numbers(input_dict, *rest_variables):
+def inc_numbers(input_dict):
     for key in input_dict:
-        if type(input_dict[key]).__name__[0].upper() == 'I':
+        if type(input_dict[key]) == int:
             input_dict[key] = input_dict[key] + 1
 
     return input_dict
