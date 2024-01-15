d179cd2a1041733e8a3ba07c8e8fa5304f9fae47
Ivan <yvan.avr1702@gmail.com>
2024-01-15 12:41:54 +0200

    Remove unused and senslees code

--- Tasks/sum.py
+++ Tasks/sum.py
@@ -1,14 +1,10 @@
 # Sum all numbers from an array
 
 def numbers_sum(input_arr):
-    sum_array = [0]
-    k = 5
+    sum_val = 0
 
-    for i in input_arr:
-        t = type(i).__name__
-        if t == 'int':
-            if len(sum_array) > 0:
-                new_sum = sum_array[-1] + i
-                sum_array.append(new_sum)
+    for element in input_arr:
+        if type(element) == int:
+            sum_val += element
 
-    return sum_array[-1]
+    return sum_val
