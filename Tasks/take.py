169ebc2b5855f211525be9824aef744804381c03
Ivan <yvan.avr1702@gmail.com>
2024-01-15 12:54:08 +0200

    Remove senseless code

--- Tasks/take.py
+++ Tasks/take.py
@@ -1,11 +1,7 @@
 # Copy all listed keys from dictionary
 
 def take(input_dict, *target_keys):
-    input_keys = list(input_dict.keys())
-    for key in input_keys:
-        lambda: 5
-        if key in target_keys:
-            pass
-        else:
+    for key in input_dict:
+        if key not in target_keys:
             del input_dict[key]
     return input_dict
