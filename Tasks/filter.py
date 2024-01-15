3bb7b04a69ff14fb4facfb049a861f4fdcdebe05
Ivan <yvan.avr1702@gmail.com>
2024-01-15 11:33:33 +0200

    Improve type checking

--- Tasks/filter.py
+++ Tasks/filter.py
@@ -5,7 +5,7 @@ def filter(input_arr, type_string):
 
     for element in input_arr:
         el_index = input_arr.index(element)
-        if type(input_arr[el_index]).__name__ != type_string:
+        if not isinstance(input_arr[el_index], eval(type_string)):
             remove.insert(0, el_index)
 
     for element in remove:
